6021a41ea4c26557a6193f1abc22c570ab1b27e1
woosaah <44954549+woosaah@users.noreply.github.com>
2025-06-14 17:38:00 +1200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,20 @@
+#crontab -e
 */5 * * * * cd /home/colle/colehub && /usr/bin/git pull >> /home/colle/git-sync.log 2>&1 && pkill -f "node index.js" && /usr/bin/node /home/colle/colehub/index.js >> /home/colle/node.log 2>&1
+
+
+#Create/Enable service
+sudo systemctl daemon-reexec
+sudo systemctl enable colehub-server.service
+sudo systemctl start colehub-server.service
+
+
+#autostart
+mkdir -p ~/.config/autostart
+nano ~/.config/autostart/colehub-kiosk.desktop
+
+
+[Desktop Entry]
+Type=Application
+Name=ColeHub Kiosk
+Exec=chromium-browser --kiosk --app=http://localhost:3000
+X-GNOME-Autostart-enabled=true
